c19518fd4ceaa7320dc835594b51d7e220d862bf
Ben Cornelis & Michael Wayne Hoeft <student@epicodus.com>
2015-05-06 15:46:43 -0700

    allow operator to add a train and edit a train's line

--- app.rb
+++ app.rb
@@ -10,3 +10,52 @@ DB = PG.connect({:dbname => "train_system"})
 get('/') do
   erb(:index)
 end
+
+get('/operator') do
+  erb(:operator)
+end
+
+get('/rider') do
+  erb(:rider)
+end
+
+get('/:user/trains') do
+  @user = params.fetch('user')
+  @trains = Train.all()
+  erb(:trains)
+end
+
+get('/:user/cities') do
+end
+
+get('/trains/new') do
+  erb(:train_form)
+end
+
+post('/operator/trains') do
+  @user = "operator"
+  line = params.fetch("line")
+  new_train = Train.new({:line => line, :id => nil})
+  new_train.save()
+  @trains = Train.all()
+  erb(:trains)
+end
+
+get('/:user/trains/:id') do
+  @train = Train.find(params.fetch("id").to_i)
+  @user = params.fetch("user")
+  erb(:train)
+end
+
+patch('/trains/:id') do
+  @train = Train.find(params.fetch("id").to_i)
+  new_line = params.fetch("new_line")
+  @train.update({:line => new_line})
+  @user = "operator"
+  erb(:train)
+end
+
+get('/trains/:id/edit') do
+  @id = params.fetch('id').to_i
+  erb(:train_edit)
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,13 +2,25 @@ require('capybara/rspec')
 require('./app')
 require "spec_helper"
 Capybara.app = Sinatra::Application
-set(:show_exceptions, false) 
+set(:show_exceptions, false)
 
 describe('creating a train', {:type => :feature}) do
   it('processes user entry to create train') do
     visit('/trains/new')
-    fill_in('name', :with => 'blue line')
+    fill_in('line', :with => 'blue line')
     click_button('submit')
-    expect(page).to have_content('Trains', 'blue line')
+    expect(page).to have_content('blue line')
+  end
+end
+
+describe('changing a train name', {:type => :feature}) do
+  it('processes operator entry to change a train name') do
+    new_train = Train.new({:line => 'purple', :id => nil})
+    new_train.save()
+    visit("/operator/trains/#{new_train.id}")
+    click_link("Edit")
+    fill_in("new_line", :with => "orange")
+    click_button("Change line")
+    expect(page).to have_content("orange")
   end
 end
