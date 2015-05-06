1ec4af8d4746d88722d73f19396659f97773dfd0
Ben Cornelis & Michael Wayne Hoeft <student@epicodus.com>
2015-05-06 11:23:37 -0700

    write save, all, find methods for City

--- lib/city.rb
+++ lib/city.rb
@@ -1,17 +1,32 @@
 class City
-  
+  attr_reader :name, :id
+
   def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
   end
 
-  def ==(other_train)
+  def ==(other_city)
+    same_name = @name.eql?(other_city.name)
+    same_id = @id.eql?(other_city.id)
+    same_name.&(same_id)
   end
 
   def save
-    #create
+    result = DB.exec("INSERT INTO cities (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
   end
 
   def self.all
-    #list and read trains
+    cities = []
+    results = DB.exec("SELECT * FROM cities;")
+    results.each() do |result|
+      name = result.fetch("name")
+      id = result.fetch("id").to_i
+      city = City.new({:name => name, :id => id})
+      cities.push(city)
+    end
+    cities
   end
 
   def update(attributes)
@@ -20,5 +35,12 @@ class City
 
   def delete
     #delete
+  end
+
+  def self.find(city_id)
+    result = DB.exec("SELECT * FROM cities WHERE id = #{city_id};")
+    name = result.first.fetch("name")
+    id = result.first.fetch("id").to_i
+    City.new({:name => name, :id => id})
   end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,1 +1,28 @@
 require "spec_helper"
+
+
+describe(City) do
+  describe("#==") do
+    it("returns true for objects that have same name and id") do
+      city1 = City.new({:name => 'Portland', :id => nil})
+      city2 = City.new({:name => 'Portland', :id => nil})
+      expect(city1.==(city2)).to(eq(true))
+    end
+  end
+
+  describe("#save") do
+    it("add the city to the cities table") do
+      city1 = City.new({:name => 'Portland', :id => nil})
+      city1.save()
+      expect(City.all()).to(eq([city1]))
+    end
+  end
+
+  describe(".find") do
+    it("returns a city with the given id") do
+      city1 = City.new({:name => 'Portland', :id => nil})
+      city1.save()
+      expect(City.find(city1.id)).to(eq(city1))
+    end
+  end
+end
